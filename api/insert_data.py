93425b5fc72012ea7be22d56234a2f19c783a4c6
JoeMcNiff <joseph.a.mcniff@gmail.com>
2025-09-25 16:33:33 -0400

    writing into csv

--- api/insert_data.py
+++ api/insert_data.py
@@ -1,2 +1,10 @@
+import csv
+
 def insert_data(file_path: str, image_class: str, group: str, weight: str):
     print(f"file path: {file_path} -- image_class:{image_class}")
+
+    with open('training.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([file_path, image_class, group, weight])
+        
+
