d7f608630b21e76e876ea7504b2aca1994f3306b
JoeMcNiff <joseph.a.mcniff@gmail.com>
2025-09-25 21:11:21 -0400

    operation data tracking

--- api/insert_data.py
+++ api/insert_data.py
@@ -3,7 +3,7 @@ import csv
 def insert_data(file_path: str, image_class: str, group: str, weight: str):
     print(f"file path: {file_path} -- image_class:{image_class}")
 
-    with open('training.csv', 'w', newline='') as csvfile:
+    with open('training.csv', 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow([file_path, image_class, group, weight])
         
